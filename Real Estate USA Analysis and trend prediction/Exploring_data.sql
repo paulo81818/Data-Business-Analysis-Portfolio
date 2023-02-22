4dabe5a102d689f4cb7a88cc4200a74d088a5aca
Pavel Liaoshka <121188650+paulo81818@users.noreply.github.com>
2023-02-22 15:03:05 +0300

    Update Exploring_data.sql

--- Real Estate USA Analysis and trend prediction/Exploring_data.sql
+++ Real Estate USA Analysis and trend prediction/Exploring_data.sql
@@ -49,16 +49,24 @@ ORDER BY
 
 /* Explore city */    
     
-SELECT
+SELECT    
     city,  
     COUNT(city) AS num_of_property,
     AVG(price) AS avg_price,
+    MIN(price) AS min_price,
+    MAX(price) AS max_price,
+    AVG(house_size_m2) AS avg_size,
+    MIN(house_size_m2) AS min_size,
+    MAX(house_size_m2) AS max_size,
+    AVG(hectare_lot) AS avg_lot,
+    MIN(hectare_lot) AS min_lot,
+    MAX(hectare_lot) AS max_lot,   
 FROM 
-    `myproject8888-357816.real_estate_us.re_us2` 
+    `myproject8888-357816.real_estate_us.re_us4`
 GROUP BY
     city
 ORDER BY 
-    num_of_property DESC
+    num_of_property DESC 
     
     
  
