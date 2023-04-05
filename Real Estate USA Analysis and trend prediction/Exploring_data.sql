24f55d5acec0e20b46788361fa9ec7acfe6fe65a
Pavel Liaoshka <121188650+paulo81818@users.noreply.github.com>
2023-04-05 16:28:40 +0300

    Update Exploring_data.sql

--- Real Estate USA Analysis and trend prediction/Exploring_data.sql
+++ Real Estate USA Analysis and trend prediction/Exploring_data.sql
@@ -159,7 +159,69 @@ GROUP BY
 ORDER BY 
     count_bed DESC
 
- 
+
+/* Adding "year" column. */
+
+CREATE OR REPLACE TABLE `myproject8888-357816.real_estate_us.re_us_property`
+AS
+SELECT 
+    *,
+    EXTRACT(YEAR FROM sold_date) AS year,
+    FROM `myproject8888-357816.real_estate_us.re_us_property` 
+    
+    
+/* Query data for Power BI exploration by year. */    
+SELECT
+    state,
+    city,
+    year,
+    bedrooms,
+    bathrooms,  
+    COUNT(state) AS num_of_property,
+    AVG(price) AS avg_price,
+    MIN(price) AS min_price,
+    MAX(price) AS max_price,
+    AVG(house_size_m2) AS avg_size,
+    MIN(house_size_m2) AS min_size,
+    MAX(house_size_m2) AS max_size,
+    AVG(hectare_lot) AS avg_lot,
+    MIN(hectare_lot) AS min_lot,
+    MAX(hectare_lot) AS max_lot,   
+FROM 
+    `myproject8888-357816.real_estate_us.re_us_property`
+WHERE year IS NOT NULL
+GROUP BY
+    state, city, year, bedrooms, bathrooms
+ORDER BY 
+    num_of_property DESC 
+
+
+
+/* Query data for Power BI exploration by year and month. */    
+SELECT
+    state,
+    city,
+    year,
+    FORMAT_DATE('%B', sold_date) AS month,
+    bedrooms,
+    bathrooms,  
+    COUNT(state) AS num_of_property,
+    AVG(price) AS avg_price,
+    MIN(price) AS min_price,
+    MAX(price) AS max_price,
+    AVG(house_size_m2) AS avg_size,
+    MIN(house_size_m2) AS min_size,
+    MAX(house_size_m2) AS max_size,
+    AVG(hectare_lot) AS avg_lot,
+    MIN(hectare_lot) AS min_lot,
+    MAX(hectare_lot) AS max_lot,   
+FROM 
+    `myproject8888-357816.real_estate_us.re_us_property`
+WHERE year IS NOT NULL
+GROUP BY
+    state, city, year, month, bedrooms, bathrooms
+ORDER BY 
+    num_of_property DESC 
  
 
 
